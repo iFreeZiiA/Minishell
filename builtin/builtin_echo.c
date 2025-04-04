5bea24d8d49aba48638001a48ef234ecbbd8f530
Alexandre Arroyo <alearroy@c2r1s6.42malaga.com>
2025-04-04 15:38:31 +0200

    add minishell

---
+++ builtin/builtin_echo.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_echo.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alearroy <alearroy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/04 14:47:21 by alearroy          #+#    #+#             */
+/*   Updated: 2025/04/04 15:35:47 by alearroy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+static int has_n_flag(char *arg)
+{
+	int i = 1;
+
+	if (!arg || arg[0] != '-')
+		return 0;
+	while (arg[i])
+	{
+		if (arg[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int builtin_echo(char **args)
+{
+	int i = 1;
+	int newline = 1;
+
+	while (args[i] && has_n_flag(args[i]))
+	{
+		newline = 0;
+		i++;
+	}
+	while (args[i])
+	{
+		ft_printf("%s", args[i]);
+		if (args[i + 1])
+			ft_printf(" ");
+		i++;
+	}
+	if (newline)
+		ft_printf("\n");
+	return (0);
+}
---
+++ minishell_executor.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell_executor.h                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alearroy <alearroy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/04 15:09:38 by alearroy          #+#    #+#             */
+/*   Updated: 2025/04/04 15:37:07 by alearroy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_EXECUTOR_H
+# define MINISHELL_EXECUTOR_H
+
+#include <stdio.h>
+#include <string.h>
+
+#endif
