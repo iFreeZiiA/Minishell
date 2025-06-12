2012becdd29384d84ab3219b5076e4a0d334cdfd
Alexandre Arroyo <alearroy@c2r1s6.42malaga.com>
2025-06-12 18:28:34 +0200

    add freelist

--- header/executor.h
+++ header/executor.h
@@ -6,7 +6,7 @@
 /*   By: alearroy <alearroy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 18:41:32 by alearroy          #+#    #+#             */
-/*   Updated: 2025/06/12 18:05:36 by alearroy         ###   ########.fr       */
+/*   Updated: 2025/06/12 18:27:15 by alearroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ int		executor_from_ast(t_ast_node *ast, t_env *env);
 void	close_pipe_and_update(int *prev, int *pipe_fd);
 void	wait_all_pids(pid_t *pids, int count, t_env *env);
 void	child_process(t_list *cmd_l, int in, int out, t_env *env);
+void	free_command_list(t_list *cmds);
 
 #endif
