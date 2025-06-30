4df72401dbd21d90afe5e99f78d4dcac5b679fbf
Alexandre Arroyo <alearroy@c2r1s6.42malaga.com>
2025-06-30 20:18:21 +0200

    Executable

--- mandatory/builtin/builtin_echo.c
+++ mandatory/builtin/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: alearroy <alearroy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 14:47:21 by alearroy          #+#    #+#             */
-/*   Updated: 2025/06/30 19:44:19 by alearroy         ###   ########.fr       */
+/*   Updated: 2025/06/30 19:47:47 by alearroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,11 @@ static int	has_n_flag(char *arg)
 
 int	builtin_echo(char **args)
 {
-	int i = 1;
-	int newline = 1;
+	int i;
+	int newline;
 
+	i = 1;
+	newline = 1;
 	while (args[i] && has_n_flag(args[i]))
 	{
 		newline = 0;
