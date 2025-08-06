902f80f2e0dd74015358722f05d29f85498df8de
M-Jorda <j.jorda@gmail.com>
2025-08-06 19:41:59 +0200

    Cleaning in progress...

--- mk/configs/Sources.mk
+++ mk/configs/Sources.mk
@@ -1,31 +1,56 @@
-# Configuration ultra-minimal pour tester SEULEMENT executor 
+# ************************************************************************** #
+#                                                                            #
+#                                                        :::      ::::::::   #
+#   Sources_new.mk                                     :+:      :+:    :+:   #
+#                                                    +:+ +:+         +:+     #
+#   By: jjorda <jjorda@student.42.fr>              +#+  +:+       +#+        #
+#                                                +#+#+#+#+#+   +#+           #
+#   Created: 2025/08/06 00:00:00 by jjorda            #+#    #+#             #
+#   Updated: 2025/08/06 00:00:00 by jjorda           ###   ########.fr       #
+#                                                                            #
+# ************************************************************************** #
 
-# ********************************* SOURCES ********************************** #
+# Sources propres basées sur les fichiers réellement compilés et fonctionnels
+
+# ********************************* CORE *********************************** #
 
 SRC_MAIN = mandatory/main.c
 
-SRC_BUILTIN = mandatory/builtin/builtin_cd.c \
-	mandatory/builtin/builtin_cd_utils.c \
-	mandatory/builtin/builtin_echo.c \
+# ******************************** BUILTIN ********************************** #
+
+SRC_BUILTIN = mandatory/builtin/builtin_cd_utils.c \
+	mandatory/builtin/builtin_pwd.c \
+	mandatory/builtin/run_builtin.c \
+	mandatory/builtin/builtin_unset.c \
+	mandatory/builtin/builtin_export.c \
 	mandatory/builtin/builtin_env.c \
 	mandatory/builtin/builtin_exit.c \
-	mandatory/builtin/builtin_export.c \
-	mandatory/builtin/builtin_pwd.c \
-	mandatory/builtin/builtin_unset.c \
-	mandatory/builtin/run_builtin.c
+	mandatory/builtin/builtin_echo.c \
+	mandatory/builtin/builtin_cd.c
+
+# ******************************* EXECUTOR ********************************* #
+
+SRC_EXECUTOR = mandatory/executor/executor.c \
+	mandatory/executor/ast_to_command_list.c \
+	mandatory/executor/get_path.c
+
+# ******************************* SIGNAL *********************************** #
 
 SRC_SIGNAL = mandatory/signal/signal.c
 
-SRC_EXECUTOR = mandatory/executor/ast_to_command_list.c \
-	mandatory/executor/executor.c \
-	mandatory/executor/get_path.c
+# ****************************** DEPLOYMENT ******************************** #
 
 SRC_DEPLOYMENT = mandatory/deployment/setup/setup.c \
+	mandatory/deployment/setup/environment.c \
 	mandatory/deployment/cleanup/cleanup.c
 
-SRC_PARSER_BASE = mandatory/parser/parser.c \
-	mandatory/parser/enhanced.c \
+# ******************************* PARSER *********************************** #
+# Parser de base
+SRC_PARSER_BASE = mandatory/parser/enhanced.c \
+	mandatory/parser/parser.c \
 	mandatory/parser/stub_functions.c
+
+# ******************************** LEXING *********************************** #
 
 SRC_LEXING = mandatory/parser/lexing/err.c \
 	mandatory/parser/lexing/utils.c \
@@ -35,30 +60,46 @@ SRC_LEXING = mandatory/parser/lexing/err.c \
 	mandatory/parser/lexing/parse_var.c \
 	mandatory/parser/lexing/expand.c
 
-SRC_PARSING_CORE = mandatory/parser/parsing/ast/create.c \
-	mandatory/parser/parsing/ast/free.c \
-	mandatory/parser/parsing/logical/logical.c \
-	mandatory/parser/parsing/logical/precedence.c \
-	mandatory/parser/parsing/parentheses/check.c \
-	mandatory/parser/parsing/parentheses/find.c
+# ******************************** PARSING ********************************** #
+# AST
+SRC_PARSING_AST = mandatory/parser/parsing/ast/create.c \
+	mandatory/parser/parsing/ast/free.c
 
-# PHASE 4-5: Ajout des modules de redirections
-SRC_REDIRECTIONS = mandatory/parser/parsing/redir/redir.c \
-	mandatory/parser/parsing/redir/input.c \
+# Opérateurs logiques
+SRC_PARSING_LOGICAL = mandatory/parser/parsing/logical/logical.c \
+	mandatory/parser/parsing/logical/precedence.c
+
+# Pipes
+SRC_PARSING_PIPES = mandatory/parser/parsing/pipes/operators.c \
+	mandatory/parser/parsing/pipes/pipes.c
+
+# Redirections
+SRC_PARSING_REDIR = mandatory/parser/parsing/redir/input.c \
 	mandatory/parser/parsing/redir/output.c \
-	mandatory/parser/parsing/redir/validate.c \
-	mandatory/parser/parsing/utils/redir.c
+	mandatory/parser/parsing/redir/redir.c \
+	mandatory/parser/parsing/redir/validate.c
 
-# Validation syntaxique
-SRC_VALIDATION = mandatory/parser/parsing/validate/validate.c \
+# Utilitaires
+SRC_PARSING_UTILS = mandatory/parser/parsing/utils/redir.c \
+	mandatory/parser/parsing/parse.c
+
+# Validation
+SRC_PARSING_VALIDATE = mandatory/parser/parsing/validate/validate.c \
 	mandatory/parser/parsing/validate/validation.c \
 	mandatory/parser/parsing/validate/validation_utils.c \
 	mandatory/parser/parsing/validate/validation_errors.c \
 	mandatory/parser/parsing/validate/validation_advanced.c
 
-# Combinaison finale AVEC redirections
+# Regroupement parsing
+SRC_PARSING = $(SRC_PARSING_AST) $(SRC_PARSING_LOGICAL) $(SRC_PARSING_PIPES) \
+	$(SRC_PARSING_REDIR) $(SRC_PARSING_UTILS) $(SRC_PARSING_VALIDATE)
+
+# ****************************** SOURCES FINALES *************************** #
+
 SRC = $(SRC_MAIN) $(SRC_BUILTIN) $(SRC_SIGNAL) $(SRC_EXECUTOR) \
-	$(SRC_DEPLOYMENT) $(SRC_PARSER_BASE) $(SRC_LEXING) $(SRC_PARSING_CORE) $(SRC_REDIRECTIONS) $(SRC_VALIDATION)
+	$(SRC_DEPLOYMENT) $(SRC_PARSER_BASE) $(SRC_LEXING) $(SRC_PARSING)
+
+# ********************************* BONUS *********************************** #
 
 BONUS_SOURCES = bonus/main.c
 
@@ -76,6 +117,6 @@ LIBMS		= $(DIR_LIBMS)/libms.a
 
 # ********************************* OBJECTS ********************************** #
 
-DIR_OBJ	= obj
-DIR		= $(DIR_OBJ)/
-OBJ		= $(patsubst %.c, $(DIR)%.o, $(SRC))
+DIR_OBJ		= obj
+DIR			= $(DIR_OBJ)/
+OBJ			= $(patsubst %.c, $(DIR)%.o, $(SRC))
